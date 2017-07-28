cbbe4424c7c4356a68b63914fd6e5157fa7fa8b5
MitchAguilar <ma.aguilar@udla.edu.co>
2017-07-28 01:52:36 -0500

    add new modules and render

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,25 @@
-require('./modulo1.js');
-require('./modulo2.js');
+import React from "react";
+import ReacDOM from "react-dom";
+//componet
+class Layout extends React.Component{//class js
+    render(){//render de js
+        return(
+            <h1>Hola Mundo <img src="https://avatars1.githubusercontent.com/u/25367106?v=4&s=460"/></h1>
+        );
+    }
+}
+class Layout2 extends React.Component{//class js
+      constructor(){
+            super();
+            this.nombre="Mitch Aguilar"; }
+    render(){//render de js
+        return(
+            <h1>Hola {this.nombre}</h1>
+        );
+    }
+}
+const app=document.getElementById('app');
+const app2=document.getElementById('app2');
+
+ReacDOM.render(<Layout/>,app);
+ReacDOM.render(<Layout2/>,app2);
